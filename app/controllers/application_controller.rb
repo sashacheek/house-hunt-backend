fabad8567aa72335300175fb06c6aa8f75181da9
Sasha Cheek <sasha@cheek.tv>
2025-05-06 15:12:03 -0600

    Adjust auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include Authentication
   include ActionController::Cookies
   before_action :authorize_request
 
--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,12 +1,12 @@
 class PasswordsController < ApplicationController
-  # allow_unauthenticated_access
+  allow_unauthenticated_access
   before_action :set_user_by_token, only: %i[ edit update ]
 
   def new
   end
 
   def create
-    if user = Lister.find_by(email_address: params[:email_address])
+    if user = User.find_by(email_address: params[:email_address])
       PasswordsMailer.reset(user).deliver_later
     end
 
@@ -26,7 +26,7 @@ class PasswordsController < ApplicationController
 
   private
     def set_user_by_token
-      @user = Lister.find_by_password_reset_token!(params[:token])
+      @user = User.find_by_password_reset_token!(params[:token])
     rescue ActiveSupport::MessageVerifier::InvalidSignature
       redirect_to new_password_path, alert: "Password reset link is invalid or has expired."
     end
