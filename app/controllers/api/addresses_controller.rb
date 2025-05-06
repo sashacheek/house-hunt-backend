f97fbd369081329167364f1210a13285b2b10683
Sasha Cheek <sasha@cheek.tv>
2025-05-06 17:56:06 -0600

    Skip auth on addresses get

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -1,5 +1,12 @@
 class Api::AddressesController < ApplicationController
-  allow_unauthenticated_access only: %i[ index show ]
+  skip_before_action :authorize_request, only: %i[index show]
+  
+    def index
+      addresses = Address.all
+      render json: addresses
+    end
+  end
+  
   def index
     addresses = Address.all
     render json: addresses
