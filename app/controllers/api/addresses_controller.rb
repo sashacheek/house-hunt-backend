3a7ecd60be8e59fa2de81daae5275843046a83f8
Sasha Cheek <sasha@cheek.tv>
2025-05-06 18:00:42 -0600

    Fix syntax errors

--- app/controllers/api/addresses_controller.rb
+++ app/controllers/api/addresses_controller.rb
@@ -1,12 +1,5 @@
 class Api::AddressesController < ApplicationController
   skip_before_action :authorize_request, only: %i[index show]
-  
-    def index
-      addresses = Address.all
-      render json: addresses
-    end
-  end
-  
   def index
     addresses = Address.all
     render json: addresses
